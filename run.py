2b910e43e39d5a9b4ebff2dd404dd459e6af906e
Alexandra Kalisz <alexkalisz@gmail.com>
2024-08-27 16:23:39 +0000

    add function to collect survey data from the user

--- run.py
+++ run.py
@@ -16,3 +16,28 @@ survey = SHEET.worksheet('survey')
 data = survey.get_all_values()
 
 print(data)
+
+def get_survey_data():
+    """
+    Get survey data input from the user.
+    Run a while loop to collect a valid string of data from the user
+    via the terminal, which must be a string of values separated
+    by commas.
+    """
+    print("Please enter survey data.")
+    print("Data should be in the format: value1,value2,value3,...,value10")
+    print("Example: 5,Yes,23,Male\n")
+
+    data_str = input("Enter your data here:\n")
+
+    print(f"The data provided is: {data_str}")
+
+def main():
+    """
+    Run all program functions.
+    """
+    get_survey_data()
+    # Other functions will be added here later
+
+print("Welcome to Survey Data Analysis")
+main()
